5934c52b73bff6490fd19b8fe015ff50a6849d76
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-13 15:51:14 +0200

    Aufgabe erledigt

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+let helloWorld = "Hello World";
+const pi = 3.14;
+const burjDubai = '828m';
+let fullName = 'Jan Schhmidt';
+const eifelTower = '324m';
+let camelCase = 'camelCase';
+let kursStatus = true;
+
